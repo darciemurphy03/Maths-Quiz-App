ae1688a163835e7198ef0c35f8dea51b58db12a4
darciemurphy03 <murphy-d72@ulster.ac.uk>
2023-11-17 12:12:14 +0000

    Halfway through Firebase setup

--- app/src/main/java/com/example/com413application/MainActivity.java
+++ app/src/main/java/com/example/com413application/MainActivity.java
@@ -1,14 +1,154 @@
 package com.example.com413application;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    Button SignOut;
+    EditText data_input;
+    Button add, remove, get;
+    TextView data_output;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        createSignInIntent();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance(Insert Your DB Ref Here!);
+        DatabaseReference myRef = database.getReference("User");
+        myRef.setValue("Hello, World!");
+
+        SignOut = (Button) findViewById(R.id.signout_btn);
+        SignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signOut();
+            }
+        });
+
+        data_input = (EditText) findViewById(R.id.data_txt);
+        data_output = (TextView) findViewById(R.id.output_txt);
+
+        add = (Button) findViewById(R.id.add_btn);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = data_input.getText().toString();
+                myRef.setValue(data);
+            }
+        });
+
+        remove = (Button) findViewById(R.id.remove_btn);
+        remove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.removeValue();
+            }
+        });
+
+        get = (Button) findViewById(R.id.get_btn);
+        get.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        String dbOutput = snapshot.getValue(String.class);
+                        data_output.setText(dbOutput);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
+
+
+
+    }
+
+    public void createSignInIntent(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.AnonymousBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result){
+        IdpResponse response = result.getIdpResponse();
+        if(result.getResultCode()==RESULT_OK){
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Toast.makeText(this, user.getEmail(), Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void signOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        createSignInIntent();
+                    }
+                });
     }
 }
