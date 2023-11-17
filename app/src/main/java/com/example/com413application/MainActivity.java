526584159e914c63d55ca900f0f996ca1f66b212
Darcie Murphy <Murphy-D72@ulster.ac.uk>
2023-11-17 23:27:02 +0000

    Navbar working to switch between pages

--- app/src/main/java/com/example/com413application/MainActivity.java
+++ app/src/main/java/com/example/com413application/MainActivity.java
@@ -4,14 +4,24 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.example.com413application.HomePage;
+import com.example.com413application.RecipesPage;
+import com.example.com413application.RandomisePage;
+import com.example.com413application.CaloriesPage;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
@@ -19,6 +29,7 @@ import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,7 +38,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
-    BottomNavigationView bottomNavigationView;
+    private static final String TAG = "MainActivity";
     private FirebaseAuth mAuth;
     Button SignOut;
 
@@ -51,7 +62,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        BottomNavigationView bottom_navigation = findViewById(R.id.bottom_navigation);
 
         mAuth = FirebaseAuth.getInstance();
         createSignInIntent();
@@ -64,17 +75,44 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-        bottomNavigationView
-                = findViewById(R.id.bottom_navigation);
+        final HomePage homePage = new HomePage();
+        final RecipesPage recipesPage = new RecipesPage();
+        final RandomisePage randomisePage = new RandomisePage();
+        final CaloriesPage caloriesPage = new CaloriesPage();
 
-        //bottomNavigationView
-                //.setOnNavigationItemSelectedListener(MainActivity.this);
-       // bottomNavigationView.setSelectedItemId(R.id.fl_wrapper);
+        setCurrentFragment(homePage);
+
+        bottom_navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_home) {
+                    setCurrentFragment(homePage);
+                    Log.i(TAG, "Home Selected");
+                } else if (itemId == R.id.nav_recipes) {
+                    setCurrentFragment(recipesPage);
+                    Log.i(TAG, "Recipes Selected");
+                } else if (itemId == R.id.nav_randomise) {
+                    setCurrentFragment(randomisePage);
+                    Log.i(TAG, "Randomise Selected");
+                } else if (itemId == R.id.nav_calorieTracker) {
+                    setCurrentFragment(caloriesPage);
+                    Log.i(TAG, "Calorie Tracker Selected");
+                }
+                return true;
+            }
+        });
+
+
     }
-    HomePage homePage = new HomePage();
-    RecipesPage recipesPage = new RecipesPage();
-    RandomisePage randomisePage = new RandomisePage();
-    CaloriesPage caloriesPage = new CaloriesPage();
+
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fl_wrapper, fragment)
+                .commit();
+    }
 
     public void createSignInIntent(){
         List<AuthUI.IdpConfig> providers = Arrays.asList(
