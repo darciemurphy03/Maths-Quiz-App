744be447ae7e10f52e7fd5664a1792619835c31a
Darcie Murphy <Murphy-D72@ulster.ac.uk>
2023-11-17 17:12:59 +0000

    Pages added but not connected to navbar yet

---
+++ app/src/main/java/com/example/com413application/HomePage.java
@@ -0,0 +1,6 @@
+package com.example.com413application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePage extends AppCompatActivity {
+}
--- app/src/main/java/com/example/com413application/MainActivity.java
+++ app/src/main/java/com/example/com413application/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mAuth = FirebaseAuth.getInstance();
         createSignInIntent();
 
